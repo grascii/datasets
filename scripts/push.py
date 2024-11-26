8301bd6c12c3e3e169bb8a8d62d6a6673a9e4876
chanicpanic <51764816+chanicpanic@users.noreply.github.com>
2024-11-25 21:20:58 -0800

    Create script to push dataset to HF

---
+++ scripts/push.py
@@ -0,0 +1,30 @@
+"""
+Push create and push a dataset to HuggingFace
+"""
+import argparse
+import sys
+
+from datasets import load_dataset
+
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("images", help="path to an image directory")
+argparser.add_argument("repo", help="HuggingFace repo to push to")
+argparser.add_argument("--revision", "-r", help="branch to push the files to")
+argparser.add_argument(
+    "--token", "-t", help="HuggingFace authentication token"
+)
+args = argparser.parse_args(sys.argv[1:])
+
+
+dataset = load_dataset(
+    "imagefolder",
+    data_dir=args.images
+)
+
+dataset.push_to_hub(
+    args.repo,
+    revision=args.revision,
+    private=True,
+    token=args.token,
+)
